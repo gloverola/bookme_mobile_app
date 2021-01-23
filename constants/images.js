0632f0305266960e3b3439da78fced96c5ec37dd
copdev <glovermartial@gmail.com>
2021-01-23 20:08:46 +0100

    first commit

---
+++ constants/images.js
@@ -0,0 +1,11 @@
+export const outsider = require( "../assets/images/outsider.jpeg" );
+export const fiveFeetApart = require( "../assets/images/five-feet-apart.jpg" );
+export const novemberRain = require( "../assets/images/november-rain.jpg" );
+export const vegetarian = require( "../assets/images/vegetarian.png" );
+
+export default {
+    outsider,
+    fiveFeetApart,
+    novemberRain,
+    vegetarian
+}
